06aaea0698afc11f946d75c3c32c5403a9fe863c
Jeferson Huayta <jefersonhuayta2o@gmail.com>
2023-09-24 14:09:11 -0500

    Agremos una anotacion correspodiente para la ruta de la plantilla con @GetMapping(aqui colocamos la ruta como ejm = '/listar')

--- src/main/java/com/CrudSpringBoot/demo/controler/Controlador.java
+++ src/main/java/com/CrudSpringBoot/demo/controler/Controlador.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.CrudSpringBoot.demo.interfaceService.inPersonaService;
@@ -13,12 +14,16 @@ import com.CrudSpringBoot.demo.modelo.Persona;
 @Controller
 @RequestMapping
 public class Controlador {
-	
+
 	@Autowired
 	private inPersonaService service;
+
+	//Annotation correspondiente @GetMapping y (Le damos una ruta)
+	@GetMapping("/listar")
 	public String listar(Model model) {
-		
-		List<Persona>personas=service.listar();
+
+		List<Persona> personas = service.listar();
+//		con la palabra model y el addAttibute enviamos la lista al formulario
 		model.addAttribute("persona", personas);
 		return "index";
 	}

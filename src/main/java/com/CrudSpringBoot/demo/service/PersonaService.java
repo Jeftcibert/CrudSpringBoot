f61b03ab3aa6576e133d7e57dbf7a4588fedd667
Jeferson Huayta <jefersonhuayta2o@gmail.com>
2023-09-24 14:42:25 -0500

    En esta parte del proyecto creamos un archivo index.html donde estaran los datos de la tabla  agregamos boostrap para el diseño unas etiquetas simples como el div table tbody thead  tr th td  y el th: each

--- src/main/java/com/CrudSpringBoot/demo/service/PersonaService.java
+++ src/main/java/com/CrudSpringBoot/demo/service/PersonaService.java
@@ -17,8 +17,8 @@ public class PersonaService implements inPersonaService{
 
 	@Override
 	public List<Persona> listar() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return (List<Persona>)data.findAll();
 	}
 
 	@Override

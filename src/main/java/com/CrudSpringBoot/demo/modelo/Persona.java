c92c6039ff243c47903428c64d8b655ac5bba7f5
Jeferson Huayta <jefersonhuayta2o@gmail.com>
2023-09-24 13:17:30 -0500

    Agregamos alguna dependencias como @Entity para que esta clase reconosca como entidad y @Table(name=persona ) para que al momento en que se creara la tabla en la BD se nombrara como la clase persona, @ID-@GeneratedValue(Strategy=GenerationType.IDENTITY-->Estamos haciendo  referencia que el AAtributo o parametro Int id sera el id de la tabla persona

--- src/main/java/com/CrudSpringBoot/demo/modelo/Persona.java
+++ src/main/java/com/CrudSpringBoot/demo/modelo/Persona.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 @Table(name = "persona")
 
 public class Persona {
-	@I
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
